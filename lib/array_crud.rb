cf01e74823e662dc755969e1f973563391295401
Camryn <camrynpatterson@yahoo.com>
2020-11-16 14:15:29 -0600

    commit tree c4a7625d062a4c2178b4e78588fa8e3064cefeb6

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,36 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+["Boxer", "Great Dane", "Bulldog", "Cane Corso"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+    array << element
+ end
 
 def add_element_to_start_of_array(array, element)
-  
+    array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
-end
+element = array.pop
+  end
 
 def remove_element_from_start_of_array(array)
-  
+  element = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+ x = array.count
+    array[x-1]
 end
